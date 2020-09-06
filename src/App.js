92906d07f21bd063def73a51f4ea87bbbbbcf7ac
Davor Ostojić <ostojic.davor@gmail.com>
2020-09-06 21:20:17 +0200

    product list (#2)
    Co-authored-by: Ostojic, Davor <davor.ostojic@bawagpsk.com>

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React from 'react';
+import data from './data.json'
+import Products from './components/Products';
 
-function App() {
+class App extends React.Component{
+
+  constructor() {
+    super();
+
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    }
+  }
+
+  render() {
+    
+
   return (
     <div className="grid-container">
       <header>
         <a href="/">React Shopping Cart </a>
       </header>
       <main>
-        Product List
+        <div className="content">
+          <div className="main">
+            <Products products={this.state.products} />
+          </div>
+          <div className="sidebar">Cart Items</div>
+         
+        </div>
       </main>
       <footer>All rigths reserved. </footer>
     </div>
   );
+  }
 }
 
 export default App;
-
-//feature 1 test
